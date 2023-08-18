30c1414080ea70def050d620b98228b69d6fa840
Killathon <huelva21@gmail.com>
2023-08-17 20:44:11 -0400

    Implemented hardware installation/deinstallation

---
+++ Source/v1.4/Recipes/Recipe_DroneDecreaseLevel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace MechHumanlikes
+{
+    public class Recipe_DroneDecreaseLevel : Recipe_SurgeryMechanical
+    {
+        // Only available for programmable drones.
+        public override bool AvailableOnNow(Thing thing, BodyPartRecord part = null)
+        {
+            if (thing is Pawn pawn && MDR_Utils.IsProgrammableDrone(pawn))
+            {
+                Hediff hardwareComplexity = pawn.health.hediffSet.GetFirstHediffOfDef(recipe.removesHediff);
+                if (hardwareComplexity != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Always "apply" to the brain as it is easy to target, and guaranteed to exist if the pawn is alive.
+        public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+        {
+            yield return pawn.health.hediffSet.GetBrain();
+        }
+
+        // Change the severity of the target hediff by negative one.
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            Hediff hardwareComplexity = pawn.health.hediffSet.GetFirstHediffOfDef(recipe.removesHediff);
+            hardwareComplexity.Severity -= 1f;
+            if (recipe.addsHediff != null)
+            {
+                pawn.health.AddHediff(recipe.addsHediff);
+            }
+            pawn.GetComp<CompReprogrammableDrone>()?.RecalculateComplexity();
+        }
+    }
+}
---
+++ Source/v1.4/Recipes/Recipe_DroneIncreaseLevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace MechHumanlikes
+{
+    public class Recipe_DroneIncreaseLevel : Recipe_SurgeryMechanical
+    {
+        // Only available for programmable drones that have the hediff to reduce.
+        public override bool AvailableOnNow(Thing thing, BodyPartRecord part = null)
+        {
+            if (thing is Pawn pawn && MDR_Utils.IsProgrammableDrone(pawn))
+            {
+                Hediff hardwareComplexity = pawn.health.hediffSet.GetFirstHediffOfDef(recipe.addsHediff);
+                if (hardwareComplexity == null || hardwareComplexity.Severity < hardwareComplexity.def.maxSeverity)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Always "apply" to the brain as it is easy to target, and guaranteed to exist if the pawn is alive.
+        public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+        {
+            yield return pawn.health.hediffSet.GetBrain();
+        }
+
+        // Change the severity of the target hediff by one.
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            Hediff hardwareComplexity = pawn.health.hediffSet.GetFirstHediffOfDef(recipe.addsHediff);
+            if (hardwareComplexity != null)
+            {
+                hardwareComplexity.Severity += 1f;
+            }
+            else
+            {
+                hardwareComplexity = HediffMaker.MakeHediff(recipe.addsHediff, pawn);
+                hardwareComplexity.Severity = 1f;
+                pawn.health.AddHediff(hardwareComplexity);
+                pawn.GetComp<CompReprogrammableDrone>()?.RecalculateComplexity();
+            }
+        }
+    }
+}
